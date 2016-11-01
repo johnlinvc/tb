031c5ee0c74f168d96814b4c39316bc5b6587e56
johnlinvc <johnlinvc@gmail.com>
2016-11-01 11:44:06 +0800

    able to exec shell sub commands

--- tb/main.swift
+++ tb/main.swift
@@ -7,6 +7,41 @@
 //
 
 import Foundation
+import AppKit
+func shell(launchPath: String, arguments: [String]) -> String
+{
+    let task = Process()
+    task.launchPath = launchPath
+    task.arguments = arguments
+    
+    let pipe = Pipe()
+    task.standardOutput = pipe
+    task.launch()
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    let stdout = FileHandle.standardOutput
+    stdout.write(data)
+    let output = String(data: data, encoding: String.Encoding.utf8)!
+    if output.characters.count > 0 {
+        //remove newline character.
+        let lastIndex = output.index(before: output.endIndex)
+        return output[output.startIndex ..< lastIndex]
+    }
+    return output
+}
 
-print("Hello, World!")
-
+func bash(command: String, arguments: [String]) -> String {
+    let env = ProcessInfo().environment
+    if env["PWD"] == nil {
+        var cmd = command
+        for arg in arguments {
+            cmd += " \(arg)"
+        }
+        return cmd
+    }
+    let whichPathForCommand = shell(launchPath: "/bin/bash", arguments: [ "-l", "-c", "which \(command)" ])
+    return shell(launchPath: whichPathForCommand, arguments: arguments)
+}
+let subArgs = CommandLine.arguments.dropFirst()
+let command = subArgs.first!
+let arguments = Array(subArgs.dropFirst())
+bash(command: command, arguments: arguments)
